3c23eff08ba60c3f50edaf4860b19359bdc31d06
francorinque <francoagustinrinquecorreo@gmail.com>
2024-07-21 14:06:42 -0300

    Feat(social urls,contact): add socials urls and contact link in buttons

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,16 @@
-import useNav from "../../hooks/useNav";
-import MenuMobile from "./MenuMobile";
-import texts from "../../utils/texts.json";
-import { BsXLg } from "react-icons/bs";
-import useMediaQuery from "../../hooks/useMediaQuery";
-import acelerate_logo from "../../../public/acelerate_logo.png";
-import { HiOutlineMenuAlt3 } from "react-icons/hi";
+import useNav from '../../hooks/useNav';
+import MenuMobile from './MenuMobile';
+import texts from '../../utils/texts.json';
+import { BsXLg } from 'react-icons/bs';
+import useMediaQuery from '../../hooks/useMediaQuery';
+import acelerate_logo from '../../../public/acelerate_logo.png';
+import { HiOutlineMenuAlt3 } from 'react-icons/hi';
 
-import "../../css/font.css";
+import '../../css/font.css';
 
 const Navbar = () => {
   const { isOpen, handleIsOpen } = useNav();
-  const isSmallScreen = useMediaQuery("(max-width: 640px)"); // Suponiendo 640px como el breakpoint para móviles
+  const isSmallScreen = useMediaQuery('(max-width: 640px)'); // Suponiendo 640px como el breakpoint para móviles
 
   return (
     <header
@@ -20,19 +20,19 @@ const Navbar = () => {
         h-[90px]  text-white 
       `}
       style={{
-        backgroundColor: "#09090B",
-        justifyContent: isSmallScreen && "start",
-        paddingInline: isSmallScreen && "0px",
+        backgroundColor: '#09090B',
+        justifyContent: isSmallScreen && 'start',
+        paddingInline: isSmallScreen && '0px',
       }}
     >
       <div
-        className="font-bold   pt-2"
-        style={{ color: "#FFFFFF", fontSize: "25px", height: "fit-content" }}
+        className='font-bold   pt-2'
+        style={{ color: '#FFFFFF', fontSize: '25px', height: 'fit-content' }}
       >
         <img
-          src="https://res.cloudinary.com/db60chvpz/image/upload/v1717141705/xmmqv1s6b0uuozbwyr5o.png"
-          alt="logo"
-          className="h-[50px]"
+          src='https://res.cloudinary.com/db60chvpz/image/upload/v1717141705/xmmqv1s6b0uuozbwyr5o.png'
+          alt='logo'
+          className='h-[50px]'
         />
       </div>
 
@@ -41,38 +41,38 @@ const Navbar = () => {
         <>
           <div>
             <a
-              href={texts["section-one"].link1}
-              className=""
-              style={{ color: "#A1A1AA" }}
+              href={texts['section-one'].link1}
+              className=''
+              style={{ color: '#A1A1AA' }}
             >
-              {texts["section-one"].item1}
+              {texts['section-one'].item1}
             </a>
           </div>
           <div>
             <a
-              href={texts["section-one"].link2}
-              className=""
-              style={{ color: "#A1A1AA" }}
+              href={texts['section-one'].link2}
+              className=''
+              style={{ color: '#A1A1AA' }}
             >
-              {texts["section-one"].item2}
+              {texts['section-one'].item2}
             </a>
           </div>
           <div>
             <a
-              href={texts["section-one"].link3}
-              className=""
-              style={{ color: "#A1A1AA" }}
+              href={texts['section-one'].link3}
+              className=''
+              style={{ color: '#A1A1AA' }}
             >
-              {texts["section-one"].item3}
+              {texts['section-one'].item3}
             </a>
           </div>
           <div>
             <a
-              href={texts["section-one"].bloglink}
-              target="_blank"
-              rel="noopener noreferrer"
-              className=""
-              style={{ color: "#A1A1AA" }}
+              href={texts['section-one'].bloglink}
+              target='_blank'
+              rel='noopener noreferrer'
+              className=''
+              style={{ color: '#A1A1AA' }}
             >
               Blog
             </a>
@@ -81,29 +81,30 @@ const Navbar = () => {
       )}
 
       <a
-        href="/"
+        href={texts['contacto-url']}
+        target='_blank'
         style={{
-          border: "1px groove #3F3F46",
-          borderRadius: "4px",
-          padding: "12px 24px 12px 24px",
-          backgroundColor: "#27272A",
-          color: "#FFFFFF",
-          fontFamily: "Space",
+          border: '1px groove #3F3F46',
+          borderRadius: '4px',
+          padding: '12px 24px 12px 24px',
+          backgroundColor: '#27272A',
+          color: '#FFFFFF',
+          fontFamily: 'Space',
         }}
-        className="btn-sm text-gray-200  hover:bg-gray-800 ml-auto"
+        className='btn-sm text-gray-200  hover:bg-gray-800 ml-auto'
       >
-        <span>{texts["section-one"].button}</span>
+        <span>{texts['section-one'].button}</span>
       </a>
 
       {/* Renderizar botón de menú móvil */}
       <button
-        className="text-2xl cursor-pointer z-[90] text-secondary flex items-center justify-between md:hidden ml-auto"
+        className='text-2xl cursor-pointer z-[90] text-secondary flex items-center justify-between md:hidden ml-auto'
         onClick={handleIsOpen}
       >
         {isOpen ? (
-          <BsXLg color="white" />
+          <BsXLg color='white' />
         ) : (
-          <HiOutlineMenuAlt3 color="white" size={40} />
+          <HiOutlineMenuAlt3 color='white' size={40} />
         )}
       </button>
       <MenuMobile isOpen={isOpen} onClick={handleIsOpen} />
---
+++ src/components/ui/SocialButtons.jsx
@@ -0,0 +1,51 @@
+import { FaInstagram } from 'react-icons/fa';
+import { FaFacebook } from 'react-icons/fa';
+import { FaLinkedin } from 'react-icons/fa';
+import { FaXTwitter } from 'react-icons/fa6';
+
+const social = [
+  {
+    id: 0,
+    url: 'https://www.instagram.com/cleverscomputer/',
+    icon: <FaInstagram />,
+  },
+  {
+    id: 1,
+    url: 'https://www.facebook.com/profile.php?id=61560260110875',
+    icon: <FaFacebook />,
+  },
+  {
+    id: 2,
+    url: 'https://www.linkedin.com/company/103264928/admin/dashboard/',
+    icon: <FaLinkedin />,
+  },
+  {
+    id: 3,
+    url: 'https://x.com/CleversComputer',
+    icon: <FaXTwitter />,
+  },
+];
+
+function SocialButtons() {
+  return (
+    <div className='flex py-10 justify-center'>
+      <ul>
+        <li className='flex gap-[60px]'>
+          {social.map((item) => (
+            <a
+              key={item.id}
+              href={item.url}
+              target='_blank'
+              rel='noreferrer'
+              className='hover:bg-[#474748] bg-gray-800 h-[28px] w-[28px] rounded-[4px] text-white text-[16px] flex justify-center items-center'
+            >
+              {item.icon}
+            </a>
+          ))}
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default SocialButtons;
